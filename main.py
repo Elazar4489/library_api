d618efafdbc497d5778701d7a99e5b0018ad34b1
Your Name <emma298450@gmail.com>
2026-06-14 09:33:25 +0300

    add logger definition

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ from database.db_connection import get_connection, create_tables
 from routes import book_routes
 from routes import member_routes
 from routes import report_routes
+import logging
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(name)s | %(massage)s")
+file_handler = logging.FileHandler("logs/app.log", encoding = "utf-8")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 app = FastAPI()
 app.include_router(book_routes.router)
 app.include_router(member_routes.router)
 app.include_router(report_routes.router)
-
